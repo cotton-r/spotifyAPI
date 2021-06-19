72cb9af70c693cdf08d35341da701d0886cf65bd
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-06-19 19:30:49 +0100

    pass updatePlaylistName method to Playlist comp as a prop

--- my-app/src/Components/App/App.js
+++ my-app/src/Components/App/App.js
@@ -65,6 +65,7 @@ class App extends React.Component {
               playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
             />
           </div>
         </div>
