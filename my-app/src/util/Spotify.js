80841866eb6b6be50fcdf244d9b0ebc119c4f71b
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-06-20 22:38:38 +0100

    if there is access token and expiry time available in URL then save them as variables, using setTimeout then clear these variables

--- my-app/src/util/Spotify.js
+++ my-app/src/util/Spotify.js
@@ -7,8 +7,17 @@ const Spotify = {
         }
 
         // check for access token match
-        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // this clears the parameters, allowing us to grab a new access token when it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
     } 
 }
 
