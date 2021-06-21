bd2d1b9bdd1cdafdafc2a81afc1b54adb29d1ea8
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-06-21 23:05:09 +0100

    update savePlaylist to clear playlist section after it has been saved, and also remove hard coded values from app

--- my-app/src/Components/App/App.js
+++ my-app/src/Components/App/App.js
@@ -12,17 +12,9 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [
-        {name: 'Gone', artist: 'Kanye West', album: 'Late Registration', id: 1},
-        {name: 'Forever ever', artist: 'Trippie Redd', album: 'Topanga', id: 2},
-        {name: 'Juice', artist: 'Chance the Rapper', album: 'Acid Rap', id: 3}
-      ],
+      searchResults: [],
       playlistName: 'My Playlist',
-      playlistTracks: [
-        {name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4},
-        {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5}, 
-        {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6},         
-      ]
+      playlistTracks: []
     };
 
     this.addTrack = this.addTrack.bind(this);
@@ -56,6 +48,12 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
   }
 
   search(term) {
